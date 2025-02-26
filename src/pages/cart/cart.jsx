5d925cccc82299983577275f470b41079f407651
ayda <aydashamma4@gmail.com>
2025-02-26 07:56:46 -0800

    styles for the shopping cart products completed

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,8 +2,11 @@ import Products from "../product/products";
 
 const Cart = () => {
     return (
-        <div>
+        <div className="container mx-auto px-4 flex justify-center">
+            <div className="flex gap-3 flex-col">
+            <h1 className="text-[#1E293B] font-bold text-xl">Products</h1>
             <Products/>
+            </div>
         </div>
     )
 }
--- src/pages/product/products.jsx
+++ src/pages/product/products.jsx
@@ -1,8 +1,27 @@
 const Products = () => {
   return (
-    <div className="container">
-       
-    </div>
+   
+      <div className="flex justify-between w-[800px] border border-[#1E293B0A] rounded-2xl p-5 items-center">
+        <div className="flex items-center gap-8">
+          <div className="border border-[#E2E8F0] rounded-lg p-3">
+            <img src="src/assets/react.svg" alt="" />
+          </div>
+          <div>
+            <h3 className="text-base text-[#1E293B] font-semibold">SumSung Galaxy S8</h3>
+            <div className="flex items-center gap-5 mt-2">
+              <span className="text-[#1E293B] text-sm">$ 399.99</span>
+              <button><img src="src/assets/Trash Can.png" alt="" /></button>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex gap-7">
+          <button className="text-[#CBD5E1] border border-[#CBD5E1] w-[25px] h-[25px] rounded-lg hover:bg-[#F97316] hover:text-white hover:border-[#F97316]">-</button>
+          <span>0</span>
+          <button className="text-[#CBD5E1] border border-[#CBD5E1] w-[25px] h-[25px] rounded-lg hover:bg-[#F97316] hover:text-white hover:border-[#F97316]">+</button>
+        </div>
+      </div>
+   
   );
 };
 
