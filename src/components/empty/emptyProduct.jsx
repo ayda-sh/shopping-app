e48db0c5527c1c20cbebd3bb24ab60661dcdcd29
ayda <aydashamma4@gmail.com>
2025-02-27 05:47:23 -0800

    delete the entire cart with clear cart button

--- src/components/empty/emptyProduct.jsx
+++ src/components/empty/emptyProduct.jsx
@@ -1,9 +1,9 @@
-const EmptyProduct = () => {
+export const EmptyProduct = () => {
     return (
         <div>
             <h1 className="text-[#0F172A] text-4xl font-bold">YOUR BAG 
                 <br />
-                <span className="text-[#64748B] text-2xl">is currently empty</span>
+                <span className="text-[#64748B] text-[14px] font-medium">is currently empty</span>
             </h1>
         </div>
     )
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,30 +1,32 @@
-import { useEffect, useReducer } from "react";
+import { useEffect, useReducer, useState } from "react";
 import Products from "../product/products";
 import { initialsState, reducer } from "./reducer";
 import { actionType } from "../../../actions";
 import { client } from "../../../lib/axios";
+import { EmptyProduct } from "../../components/empty/emptyProduct";
 
 const Cart = () => {
   const [state, dispatch] = useReducer(reducer, initialsState);
   const { items, isError, isLoading } = state;
+  const [isCartEmpty, setIsCartEmpty] = useState(false); 
 
-  const getProductsFromApi = async () => {  
-    try {  
-      dispatch({ type: actionType.SET_LOADING, payload: true });  
-      const response = await client.get("/products");  
-      const products = response.data.data.map((item) => ({  
-        id: item.id,  
-        ...item.attributes, 
-      }));   
-      dispatch({ type: actionType.SET_ITEM, payload: products });  
-      dispatch({ type: actionType.SET_ORIGINAL_ITEM, payload: products });  
-    } catch (error) {  
-      console.log(error);  
-      dispatch({ type: actionType.SET_ERROR, payload: true });  
-    } finally {  
-      dispatch({ type: actionType.SET_LOADING, payload: false });  
-    }  
-  };  
+  const getProductsFromApi = async () => {
+    try {
+      dispatch({ type: actionType.SET_LOADING, payload: true });
+      const response = await client.get("/products");
+      const products = response.data.data.map((item) => ({
+        id: item.id,
+        ...item.attributes,
+      }));
+      dispatch({ type: actionType.SET_ITEM, payload: products });
+      dispatch({ type: actionType.SET_ORIGINAL_ITEM, payload: products });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: actionType.SET_ERROR, payload: true });
+    } finally {
+      dispatch({ type: actionType.SET_LOADING, payload: false });
+    }
+  };
 
   useEffect(() => {
     getProductsFromApi();
@@ -38,24 +40,40 @@ const Cart = () => {
     return <p>Error...</p>;
   }
 
+  const clearCart = () => {
+    dispatch({ type: actionType.CLEAR_CART });
+    setIsCartEmpty(true);
+  };
+
   return (
     <main className="container mx-auto px-4 flex justify-center">
       <div className="flex gap-3 flex-col">
         <h1 className="text-[#1E293B] font-bold text-xl">Products</h1>
-        {items.map((item) => {   
+        {items.map((item) => {
           return <Products key={item.id} {...item} />;
         })}
 
-        <hr />
-        <div>
-          <div>
-            <h4>Total</h4>
-          </div>
-          <div>
-            <span></span>
-          </div>
+        {!isCartEmpty && (
+          <>
+            <hr />
+            <div>
+              <div>
+                <h4>Total</h4>
+              </div>
+              <div>
+                <span></span>
+              </div>
+            </div>
+          </>
+        )}
+
+        <div className="my-8 flex justify-center">
+          {items.length > 0 ? (
+            <button onClick={clearCart}>Clear Cart</button>
+          ) : (
+            <EmptyProduct />
+          )}
         </div>
-        <button>Clear Cart</button>
       </div>
     </main>
   );
