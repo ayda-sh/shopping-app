b34db749529567b9b81b740633cdf8118463af82
ayda <aydashamma4@gmail.com>
2025-02-27 06:27:48 -0800

    A function created to display the total cart price

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -45,23 +45,27 @@ const Cart = () => {
     setIsCartEmpty(true);
   };
 
+  const totalAmount  = items.reduce((total, item) => {
+    return total + (Number(item.price) || 0);
+  }, 0)
+
   return (
     <main className="container mx-auto px-4 flex justify-center">
       <div className="flex gap-3 flex-col">
         <h1 className="text-[#1E293B] font-bold text-xl">Products</h1>
-        {items.map((item) => {
+        {items.slice(0, 4).map((item) => {
           return <Products key={item.id} {...item} />;
         })}
 
         {!isCartEmpty && (
           <>
             <hr />
-            <div>
+            <div className="flex justify-between">
               <div>
-                <h4>Total</h4>
+                <h4 className="text=[#1E293B] text-2xl font-bold">Total</h4>
               </div>
               <div>
-                <span></span>
+                <span className="bg-[#F97316] p-1 text-white rounded font-bold">${totalAmount}</span>
               </div>
             </div>
           </>
@@ -69,7 +73,7 @@ const Cart = () => {
 
         <div className="my-8 flex justify-center">
           {items.length > 0 ? (
-            <button onClick={clearCart}>Clear Cart</button>
+            <button className="bg-[#CBCDCD] px-2 py-1 rounded" onClick={clearCart}>Clear Cart</button>
           ) : (
             <EmptyProduct />
           )}
