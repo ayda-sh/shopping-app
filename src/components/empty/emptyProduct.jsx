2612104f6c9dfacf8082e9a8d183aa1611a9720c
ayda <aydashamma4@gmail.com>
2025-02-27 05:26:13 -0800

    Added emptyProduct.jsx component

---
+++ src/components/empty/emptyProduct.jsx
@@ -0,0 +1,10 @@
+const EmptyProduct = () => {
+    return (
+        <div>
+            <h1 className="text-[#0F172A] text-4xl font-bold">YOUR BAG 
+                <br />
+                <span className="text-[#64748B] text-2xl">is currently empty</span>
+            </h1>
+        </div>
+    )
+}
